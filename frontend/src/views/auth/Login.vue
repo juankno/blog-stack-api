df58a7645dc20eacb01a973b6c8afd6368832c12
Juan Esteban <juanes0202@gmail.com>
2021-11-24 17:14:16 -0500

    creando form para el login

--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -1,26 +1,164 @@
 <template>
- <div class="container mx-auto">
+  <div
+    class="
+      min-h-full
+      flex
+      items-center
+      justify-center
+      py-12
+      px-4
+      sm:px-6
+      lg:px-8
+    "
+  >
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <img
+          class="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+          alt="Workflow"
+        />
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              name="email"
+              v-model="user.email"
+              type="email"
+              autocomplete="email"
+              required=""
+              class="
+                appearance-none
+                rounded-none
+                relative
+                block
+                w-full
+                px-3
+                py-2
+                border border-gray-300
+                placeholder-gray-500
+                text-gray-900
+                rounded-t-md
+                focus:outline-none
+                focus:ring-indigo-500
+                focus:border-indigo-500
+                focus:z-10
+                sm:text-sm
+              "
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              name="password"
+              v-model="user.password"
+              type="password"
+              autocomplete="current-password"
+              required=""
+              class="
+                appearance-none
+                rounded-none
+                relative
+                block
+                w-full
+                px-3
+                py-2
+                border border-gray-300
+                placeholder-gray-500
+                text-gray-900
+                rounded-b-md
+                focus:outline-none
+                focus:ring-indigo-500
+                focus:border-indigo-500
+                focus:z-10
+                sm:text-sm
+              "
+              placeholder="Password"
+            />
+          </div>
+        </div>
 
-    <h3>login</h3>
-    <p v-if="loading">Loading...</p>
-    <p v-if="error" style="color: red">{{ error }}</p>
-    <input
-      type="email"
-      name="email"
-      placeholder="email"
-      v-model="user.email"
-      id="email"
-    />
-    <br />
-    <input
-      type="password"
-      name="password"
-      placeholder="password"
-      v-model="user.password"
-      id="password"
-    />
-    <br />
-    <button @click.prevent="login">Login</button>
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              v-model="user.remember_me"
+              class="
+                h-4
+                w-4
+                text-indigo-600
+                focus:ring-indigo-500
+                border-gray-300
+                rounded
+              "
+            />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+              Remember me
+            </label>
+          </div>
+
+          <div class="text-sm">
+            <a
+              href="#"
+              class="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Forgot your password?
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            @click.prevent="login"
+            class="
+              group
+              relative
+              w-full
+              flex
+              justify-center
+              py-2
+              px-4
+              border border-transparent
+              text-sm
+              font-medium
+              rounded-md
+              text-white
+              bg-indigo-600
+              hover:bg-indigo-700
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-indigo-500
+            "
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+
+            </span>
+            Sign in
+          </button>
+        </div>
+
+           <div class="text-sm" v-if="error">
+            <small
+              class="font-medium text-red-600 hover:text-red-500"
+            >
+              {{ error }}
+            </small>
+          </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -31,7 +169,8 @@ export default {
     return {
       user: {
         email: null,
-        password: null
+        password: null,
+        remember_me: false
       },
       loading: false,
       error: null
@@ -47,7 +186,7 @@ export default {
         await this.$router.push({ name: 'posts' })
       } catch (error) {
         console.log(error.response)
-        this.error = error.response.data.errors
+        this.error = error.response.data.errors ?? error
       } finally {
         this.loading = false
       }
